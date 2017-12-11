67a8cdec5a7a6a3db3f8d2ae1f67ca3d0b02363d
ggljzr <gogo.lejzr@gmail.com>
2017-12-11 22:28:36 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MI-MCS
+
+Parallel prime calculation.
---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <math.h>
+
+int* sieve(int n){
+    int * primes = new int[n];
+    int i = 1;
+    int p = 3;
+    primes[0] = 2;
+
+    while(i < n){
+        for(int j = 0; j < i; j++){
+            if (p % primes[j] == 0) break;
+            if (p < primes[j] * primes[j]){
+                primes[i] = p;
+                i++;
+                break;
+            }
+        }
+        p += 2;
+    }
+
+    return primes;
+}
+
+int main(int argc, char ** argv){
+
+    int n = 1000000;
+
+    int * primes = sieve(n);
+
+    printf("%d\n", primes[464]);
+    return 0;
+}
