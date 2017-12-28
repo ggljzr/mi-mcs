338acbee1a1de8157ae7d123efe09bcaedc98dcf
ggljzr <gogo.lejzr@gmail.com>
2017-12-28 02:55:38 +0100

    sieve working with odd numbers only

--- primes2.cpp
+++ primes2.cpp
@@ -21,27 +21,36 @@
 //but more memory intesive
 void find_primes(unsigned int n)
 {
+    unsigned int sieve_size = (n / 2);
+    char * sieve = new char[sieve_size];
+    //sieve[0:sieve_size] = 1; //slower than cilk_for
 
-    char * sieve = new char[n + 1];
-    //sieve[0:n + 1] = 1; //slower than cilk_for
-
-    cilk_for(int i = 0; i < n + 1; i++)
+    cilk_for(int i = 0; i < sieve_size; i++)
         sieve[i] = 1;
 
     int sqrtn = (int) sqrt(n);
 
-    for(unsigned int i = 2; i < sqrtn; i++)
+    for(unsigned int i = 3; i <= sqrtn; i += 2)
     {
-        if(sieve[i] == 1)
+        if(sieve[i / 2] == 1)
         {
             /*
             it seems optimal to parallelize this cycle only
             outter cycle does not have enough iterations
             that do something
             */
-            cilk_for(unsigned int p = i * i; p <= n; p += i)
+
+            /*
+            times two so we get only
+            odd multiples of i,
+            i is odd => i*i is also odd =>
+            we need to skip every other multiple
+            to get odd multiples
+            */
+            unsigned int inc = 2 * i; //its not p += 2 * i because cilk wont compile it for some reason
+            cilk_for(unsigned int p = i * i; p <= n; p += inc)
             {
-                sieve[p] = 0;
+                sieve[p / 2] = 0;
             }
         }
     }
@@ -60,7 +69,7 @@ void find_primes(unsigned int n)
 
     for(unsigned int i = n; i >= 3; i--)
     {
-    	if(sieve[i] == 1)
+    	if(sieve[i / 2] == 1)
     	{
     		max_prime = i;
     		break;
@@ -114,7 +123,7 @@ void find_primes(unsigned int n)
 
     *hyper_out << 2 << std::endl;
     cilk_for(unsigned int i = 3; i <= max_prime; i+= 2){
-        if(sieve[i] == 1)
+        if(sieve[i / 2] == 1)
         {
             *hyper_out << i << std::endl;
             *primes_count += 1;
